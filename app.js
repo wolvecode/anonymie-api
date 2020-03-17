23e1a121995f30670b04ff84f10c4408f661d5a1
wolvecode <wolvecode@gmail.com>
2020-03-17 08:09:52 +0100

    add signin signup middleware with passport

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const config = require('config')
 const path = require('path')
 const passport = require('passport')
@@ -9,26 +10,38 @@ const Joi = require('joi') // remove it, not being used.
 const suggestion = require('./router/suggestion')
 const comment = require('./router/comment')
 const admin = require('./router/admin')
-const auth = require('./router/auth')
+const login = require('./router/login')
+const secureRoute = require('./router/secure-route')
+// const auth = require('./router/auth')
 const connect = require('./connect')
 
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.')
-  process.exit(1)
-}
+// if (!config.get('jwtPrivateKey')) {
+//   console.error('FATAL ERROR: jwtPrivateKey is not defined.')
+//   process.exit(1)
+// }
 
-app.use(
-  session({
-    secret: '_anoymie',
-    saveUnInitialized: true,
-    resave: true
-  })
-)
+// app.use(
+//   session({
+//     secret: '_anoymie',
+//     saveUnInitialized: true,
+//     resave: true
+//   })
+// )
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// app.use(passport.initialize())
+// app.use(passport.session())
+// app.use(express.json())
+require('./middleware/auth')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', login)
+app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute)
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({ error: err })
+})
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
@@ -40,6 +53,6 @@ app.get('/', (req, res) => {
 app.use('/suggestion', suggestion)
 app.use('/comment', comment)
 app.use('/admin', admin)
-app.use('/auth', auth)
+// app.use('/auth', auth)
 
 module.exports = app
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,84 @@
-const jwt = require('jsonwebtoken')
-const config = require('config')
+const passport = require('passport')
+const localStrategy = require('passport-local').Strategy
+const { Admin, validate } = require('../model/admin')
+const JWTstrategy = require('passport-jwt').Strategy
+const ExtractJWT = require('passport-jwt').ExtractJwt
 
-module.exports = function(req, res, next) {
-  const token = req.header('x-auth-token')
-  if (!token) return res.status(401).send('Access denied. No token provided')
+//create a passport midddleware to handle admin registration
+passport.use(
+  'signup',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (email, password, done) => {
+      try {
+        const admin = await Admin.create({ email, password })
+        return done(null, admin)
+      } catch (error) {
+        done(error)
+      }
+    }
+  )
+)
 
-  try {
-    const decoded = jwt.verify(token, config.get('jswPrivateKey'))
-    req.admin = decoded
-    next()
-  } catch (ex) {
-    res.status(400).send('Invalid token')
-  }
-}
+//create a middleware to handle admin login
+passport.use(
+  'login',
+  new localStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async (email, password, done) => {
+      try {
+        const admin = await Admin.findOne({ email })
+        if (!admin) {
+          return done(null, false, { message: 'admin not found' })
+        }
+        //if admin, then validate password
+        const validate = await admin.isValidPassword(password)
+        if (!validate) {
+          return done(null, false, { message: 'Wrong password, try again' })
+        }
+        //if password correspond with email, then sedn
+        return done(null, admin, { message: 'login successful' })
+      } catch (error) {
+        return done(error)
+      }
+    }
+  )
+)
+
+passport.use(
+  new JWTstrategy(
+    {
+      secretOrKey: 'top_secret',
+      jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user)
+      } catch (error) {
+        done(error)
+      }
+    }
+  )
+)
+
+// const jwt = require('jsonwebtoken')
+// const config = require('config')
+
+// module.exports = function(req, res, next) {
+//   const token = req.header('x-auth-token')
+//   if (!token) return res.status(401).send('Access denied. No token provided')
+
+//   try {
+//     const decoded = jwt.verify(token, config.get('jswPrivateKey'))
+//     req.admin = decoded
+//     next()
+//   } catch (ex) {
+//     res.status(400).send('Invalid token')
+//   }
+// }
