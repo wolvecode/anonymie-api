ef0323a0907d451c9938eeeee5de5c2f8189b2c3
wolvecode <wolvecode@gmail.com>
2020-03-17 08:13:14 +0100

    admin route

--- router/admin.js
+++ router/admin.js
@@ -6,11 +6,11 @@ const router = express.Router()
 const { Admin, validate } = require('../model/admin')
 const { getAllAdmin, addAdmin } = require('../controllers/adminController')
 
-router.get('/me', auth, async (req, res) => {
-  const admin = await Admin.findById(req.admin._id).select('-password')
-  console.log(admin)
-  res.send(admin)
-})
+// router.get('/me', auth, async (req, res) => {
+//   const admin = await Admin.findById(req.admin._id).select('-password')
+//   console.log(admin)
+//   res.send(admin)
+// })
 
 router.get('/', getAllAdmin)
 
