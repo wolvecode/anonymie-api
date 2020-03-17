f283177531c06ef4fa7cf3b6818ad488d7a912e4
wolvecode <wolvecode@gmail.com>
2020-03-17 08:10:35 +0100

    modify admin model

--- model/admin.js
+++ model/admin.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const Joi = require('joi')
 const jwt = require('jsonwebtoken')
 const config = require('config')
+const Schema = mongoose.Schema
 
 const adminSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255
-  },
   email: {
     type: String,
     required: true,
@@ -25,17 +21,27 @@ const adminSchema = new mongoose.Schema({
   }
 })
 
-adminSchema.methods.generateAuthToken = function() {
-  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'))
-  return token
+adminSchema.pre('save', async function(next) {
+  const user = this
+  const hash = await bcrypt.hash(this.password, 10)
+  this.password = hash
+  next()
+})
+
+adminSchema.methods.isValidPassword = async function(password) {
+  const user = this
+  const compare = await bcrypt.compare(password, user.password)
+  return compare
 }
+
+const Admin = mongoose.model('Admin', adminSchema)
+// adminSchema.methods.generateAuthToken = function() {
+//   const token = jwt.sign({_id: this._id}, config.get('jwtPrivateKey'))
+//   return token
+// }
 
 function validateAdmin(admin) {
   const schema = {
-    name: Joi.string()
-      .min(5)
-      .max(255)
-      .required(),
     email: Joi.string()
       .min(5)
       .max(255)
@@ -47,7 +53,6 @@ function validateAdmin(admin) {
   }
   return Joi.validate(admin, schema)
 }
-const Admin = mongoose.model('Admin', adminSchema)
 
 module.exports.Admin = Admin
 module.exports.validate = validateAdmin
