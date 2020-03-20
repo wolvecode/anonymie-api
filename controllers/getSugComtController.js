4c7097ce51a9686f7c482e250639d622199a75bb
wolvecode <wolvecode@gmail.com>
2020-03-20 22:35:05 +0100

    changes

--- controllers/getSugComtController.js
+++ controllers/getSugComtController.js
@@ -1,28 +1,30 @@
-const { Suggestion, validate} = require('../model/suggestion')
-const Comment = require('../model/comment')
-const _ = require('lodash')
-const bcrypt = require('bcrypt')
+const { Suggestion, validate } = require('../model/suggestion')
+const { Comment, validate } = require('../model/comment')
 
-exports.getComtBySugId = async (req, res, next) => {
-    //get a suggestion id
-    //get a comment by the suggestion id
-    try {
-        const suggestion = await Suggestion.findById(req.params.id)
-        if(!suggestion){
-            return res.status(404).send('The suggestion with given Id was not found')
-        }
-        try{
 
-        }
-      } catch (err) {
-        console.log(err.message)
-      }
+exports.getComtSugId = (req, req, next) => {
+  const suggestion = await Suggestion.findById(req.params.id)
+  if(!suggestion) return res.status(404).send('Suggestion id not found')
+
+  const comment = await Comment.find()
+  res.send(comment)
 }
 
+exports.createCommentBySugId = (req, res, next) => {
+  //get a suggestion id
+  //create a comment base on the id
+  //save to the database base on the suggestion id attached to the comment
+  const suggestion = await Suggestion.findById(req.params.id)
+  if(!suggestion) return res.status(404).send('Suggestion id not found')
 
-exports.createCommentBySugId = (req, res, next) =>{
-    //get a suggestion id
-    //create a comment base on the id
-    //save to the database base on the suggestion id attached to the comment
+  const { error } = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
+  let comment = new Comment({
+    title: req.body.title,
+    description: req.body.description
+  })
+
+  comment = await comment.save()
+  res.send(comment)
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
 //Starting server at port 5000
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 7777
 app.listen(port, () => console.log(`Listening on port ${port}...`))
